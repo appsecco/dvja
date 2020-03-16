6d4e406b47cfd1834598117403ee17ea6402f498
Gem Immanuel <25984676+gemgit7@users.noreply.github.com>
2020-03-16 12:44:04 -0400

    Update Constant.java

--- src/main/java/com/appsecco/dvja/Constant.java
+++ src/main/java/com/appsecco/dvja/Constant.java
@@ -4,4 +4,5 @@ public class Constant {
     public static String SESSION_USER_HANDLE = "USER";
     int a=36; // Demonstration for customers
     String a = "Nah!";
+    String password = "abc123";
 }
