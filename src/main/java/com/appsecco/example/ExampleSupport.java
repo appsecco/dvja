810250e57773ae8ac862c35ace356323a6d4f0a7
Gem Immanuel <25984676+gemgit7@users.noreply.github.com>
2020-08-17 12:32:23 -0400

    Update ExampleSupport.java

--- src/main/java/com/appsecco/example/ExampleSupport.java
+++ src/main/java/com/appsecco/example/ExampleSupport.java
@@ -27,5 +27,5 @@ import com.opensymphony.xwork2.ActionSupport;
  * Base Action class for the Tutorial package.
  */
 public class ExampleSupport extends ActionSupport {
-  int a = 2;
+  int a = 3;
 }
