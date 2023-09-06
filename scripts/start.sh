6b98f504a57c7596345560d8d841ce6156e8b62f
ausmanki <100470886+ausmanki@users.noreply.github.com>
2023-09-06 16:02:49 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,5 +6,5 @@ sleep 10
 mysql -h mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "CREATE DATABASE dvja CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
 mysql -h mysql -u $MYSQL_USER -p$MYSQL_PASSWORD dvja < ./db/schema.sql
 
-mvn jetty:run
+mvn jetty:run 
  
