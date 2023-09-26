abd70325436619d333b561505a0c5c8c5328ae83
Rohit Kumar <133094046+rohitcodergroww@users.noreply.github.com>
2023-09-26 13:34:00 +0530

    Create test53434.py

---
+++ test53434.py
@@ -0,0 +1,10 @@
+## this is first file
+sources:
+  redis:
+    redis1:
+      host: 127.0.0.1
+  s3:
+    s3_1:
+      access_key: AKIA5GW2DKEWRAPUA4N5
+      secret_key: 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
+      bucket_name: test-bucket-shit1
